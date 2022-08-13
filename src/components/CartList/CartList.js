19205f7b029884b7b1352ed336ab732eee9c028e
federicopergola <fede.pergola@hotmail.com>
2022-08-13 00:28:05 -0300

    Feat | Modifications

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -1,52 +1,76 @@
 import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
+import { Link } from "react-router-dom"
 import "./CartList.css"
 
 
-const CartList = () =>{
-    const { CartProducts} = useContext(CartContext)
+const CartList = () => {
+    const { cartProducts, clearAll, clearProduct, totalCart } = useContext(CartContext)
 
-    return(
-        <>
-        {/* {CartProducts.map((product) =>{
-            return(
-             <>   */}
-            {console.log(CartProducts)};
-            <h2 className="text-center">Carrito de compras</h2>
-                <div className="card mb-3 fs-4 container mt-5">
-                    <div className="row g-12">
-                        <div className="col-md-2">
-                            <img src= {`../assests/mate1.jpeg`} class="cart-img" alt="..."></img>
-                        </div>
-                        <div className="col-md-5">
-                            <div className="card-body">
-                                <div className=" text-start row">
-                                    <p className="card-text">Mate 1</p>
-                                    <span>$1500</span>
-                                    <span>Cantidad: 1</span>
+    return (
+        <div>
+            { cartProducts.length > 0 ? 
+                <><h2 className="text-center mt-3">Carrito de compras</h2>
+
+                { cartProducts.map((product) => {
+                    let totalPrice = product.price * product.count;
+
+                    return(
+                        
+                        <div className="card mb-3 fs-4 container mt-3">
+                            <div className="row g-12">
+                                <div className="col-md-2">
+                                    <img src= {`../assests/${product.img}`} class="cart-img" alt="..."></img>
+                                </div>
+                                <div className="col-md-5">
+                                    <div className="card-body">
+                                        <div className=" text-start row">
+                                            <p className="card-text">{product.title}</p>
+                                            <span>${product.price}</span>
+                                            <span>Cantidad:{product.count}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card-body">
+                                        <div className="text-center pe-5">
+                                            <p className="card-text">Total</p>
+                                            <span className="fs-3">${totalPrice}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-1 d-flex align-self-center">
+                                    <button className="button-trash" onClick={()=>clearProduct(product.id)}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                                    </svg></button>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-4">
-                            <div className="card-body">
-                                <div className="text-center pe-5">
-                                    <p className="card-text">Total</p>
-                                    <span className="fs-3">$ 1500</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-1 d-flex align-self-center">
-                            <button className="button-trash"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-                            </svg></button>
-                        </div>
+                                                
+                    )
+
+                }) }
+
+                <div className="row g-12 w-100 mt-5 mb-5">
+                    <div className="col-md-5 text-center">
+                        <button className="btn btn-dark" onClick={() => clearAll()}>Borrar todo</button>
+                    </div>
+                    <div className="col-md-4 text-center">
+                        Total final: ${ totalCart }
                     </div>
                 </div>
-            </>  
-            )}
-    //     })}
-    // </>
-//     )
-// }
+                </>
+                : 
+                <>
+                    <div className="row text-center">              
+                        <h2 className="p-3">Carrito vacio</h2>
+                        <h3><Link to="/">Volver</Link></h3>
+                    </div>
+                </>
+            }
+        </div>
+    )
+}  
+
 
 export default CartList
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,22 +2,65 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext()
 
-const CartProvider = ({children}) =>{
+
+const CartProvider = ({children}) => { 
     const [cartProducts, setCartProducts] = useState([])
-    console.log(cartProducts);
-   
-    const addProductToCart = (product) =>{
-        setCartProducts([...cartProducts, product])
+    const [totalCart, setTotalCart] = useState(0)
+    let [totalProducts, setTotalProducts] = useState(0)
+
+    const addProductToCart = (product, ItemCount) => {
+        let isProductInCart = cartProducts.find(
+            (productInCart) => productInCart.id === product.id
+        );
+
+        if(isProductInCart) {
+            if(isProductInCart.contador + ItemCount > isProductInCart.stock)
+            
+            return false; 
+            isProductInCart.contador += ItemCount
+            setTotalProducts(isProductInCart.contador + totalProducts)
+            
+        } else {
+            product.contador = ItemCount;
+            setTotalProducts(ItemCount + totalProducts)
+            
+            setCartProducts ([...cartProducts, product]);
+        }   
+
+        setTotalCart(
+            totalCart + product.price * product.contador
+        )
     }
 
+    const clearAll = () => {
+        setCartProducts([])
+        setTotalProducts(0)
+        setTotalCart(0)
+    }
+
+    const clearProduct = ( id ) => {
+        const prod = cartProducts.find((product) => product.id === id)
+        setTotalCart(
+            totalCart - prod.price * prod.contador
+        )
+
+        setTotalProducts(totalProducts - prod.contador)
+
+        const newCart = cartProducts.filter((product) => product.id !== id)
+        setCartProducts(newCart);
+    }
 
     const data = {
-        cartProducts, 
-        setCartProducts, 
-        addProductToCart,
+        cartProducts,
+        setCartProducts,
+        clearAll,
+        totalCart,
+        totalProducts,
+        clearProduct,
+        addProductToCart
     }
 
-    return(
+    return (
         <CartContext.Provider value={data}>
             {children}
         </CartContext.Provider>
